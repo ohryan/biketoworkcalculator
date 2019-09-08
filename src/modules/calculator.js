abeb14c7497a239759b7a5a5cf974abc2250495d
Ryan Neudorf <ryan.neudorf@gmail.com>
2019-09-07 20:01:04 -0500

    Fixed US calculations!

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -4,7 +4,7 @@ export const Calc = {
     gipL: 4.54609, // imperial gallons per litre
     gpL: 3.7854,  // US gallons per litre
     mpk: 1.609344, // miles per km
-    co2L: 2.3, // grams co2 produced by burning 1L gasoline. source: https://www.nrcan.gc.ca/sites/www.nrcan.gc.ca/files/oee/pdf/transportation/fuel-efficient-technologies/autosmart_factsheet_6_e.pdf
+    co2L: 2.29, // grams co2 produced by burning 1L gasoline. source: https://www.nrcan.gc.ca/sites/www.nrcan.gc.ca/files/oee/pdf/transportation/fuel-efficient-technologies/autosmart_factsheet_6_e.pdf
     // convert fuel economy to L/km
     ecoLKm(economy, economyUnit) {
         switch(economyUnit) {
@@ -22,16 +22,23 @@ export const Calc = {
                 return distance * this.mpk;
         }
     },
+    fuelPricePerLitre(fuelPrice, fuelPriceUnit) {
+        if (fuelPriceUnit === 'gal') {
+            return fuelPrice * this.gpL;
+        }
+
+        return fuelPrice;
+    },
     // return fuel consumption in 
     fuelConsumptionInLitres(distance, distanceUnit, economy, economyUnit) {
         if (distanceUnit === 'km' && economyUnit == 'l100') {
             return (distance / 100) * economy;
         } else if (distanceUnit === 'km' && economyUnit === 'mpg') {
-            return (distance * this.mpk) * economy / this.gpL;
+            return (distance / this.mpk) * economy / this.gpL;
         } else if (distanceUnit === 'mi' && economyUnit === 'l100') {
             return (distance * this.mpk / 100) * economy;
         } else if (distanceUnit === 'mi' && economyUnit === 'mpg') {
-            return distance * economy / this.gpL;
+            return distance / economy / this.gpL;
         }
     },
     fuelCostFromLitres(fuelCost, fuelConsumptionInLitres, economyUnit) {
